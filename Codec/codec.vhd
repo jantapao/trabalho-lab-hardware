9702243efb91409937b5dce31dc7cda751d3f556
Keven Augusto <kevenbezerra@gmail.com>
2022-11-15 21:47:50 -0400

    Implementa o Codec em apenas um processo

--- Codec/codec.vhd
+++ Codec/codec.vhd
@@ -19,19 +19,20 @@ end codec;
 
 architecture algorithm of codec is
 begin
-  leitura: process (interrupt, read_signal, write_signal) is
+  processao : process (interrupt, read_signal, write_signal, codec_data_in) is
     type t_arq is file of std_logic_vector ;
     file arq_sinais : t_arq ;
     variable file_data_out : std_logic_vector(7 downto 0) ;
     variable status : file_open_status ;
     variable tam_codec : natural ;
+    variable exit_code : std_logic ;
 
   begin
     if interrupt = '1' and read_signal = '1' and write_signal = '0' then
       file_open(status, arq_sinais, "arquivo.dat", read_mode) ;
       if status /= open_ok then
         report "Erro na abertura do arquivo" severity warning ;
-        valid <= '0' ;
+        exit_code := '0' ;
       else
         while not endfile(arq_sinais) loop
           read(arq_sinais, file_data_out, tam_codec) ;
@@ -40,31 +41,25 @@ begin
           else
             if tam_codec <= file_data_out'length then
               codec_data_out <= file_data_out ;
-              valid <= '1' ;
+              exit_code := '1' ;
             end if ;
           end if ;
         end loop;
         file_close(arq_sinais) ;
       end if ;
     end if ;
-  end process leitura ;
-
-  escrita: process (interrupt, read_signal, write_signal, codec_data_in) is
-    type t_arq is file of std_logic_vector ;
-    file arq_sinais : t_arq ;
-    variable status : file_open_status ;
-  begin
     if interrupt = '1' and write_signal = '1' and read_signal = '0' then
       file_open(status, arq_sinais, "arquivo.dat", write_mode) ;
       if status /= open_ok then
         report "Erro na abertura do arquivo" severity warning ;
-        valid <= '0' ;
+        exit_code := '0' ;
       else
         write(arq_sinais, codec_data_in) ;
-        valid <= '1' ;
+        exit_code := '1' ;
         file_close(arq_sinais) ;
       end if ;
     end if ;
-  end process escrita ;
+    valid <= exit_code ;
+  end process processao ;
 
 end algorithm ;
